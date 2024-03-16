0ee0a49c628761c252684989fe925deda0ead7ac
Josh <144963859+JoshuaBarrantesMena@users.noreply.github.com>
2024-03-15 22:08:14 -0600

    deletable multiple task
    can select multiple task and delete the selected group

--- src/taskmanagement/FXMLDocumentController.java
+++ src/taskmanagement/FXMLDocumentController.java
@@ -5,7 +5,9 @@
 package taskmanagement;
 
 import java.net.URL;
+import java.util.HashSet;
 import java.util.ResourceBundle;
+import java.util.Set;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -14,6 +16,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
 /**
@@ -68,16 +71,20 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     public void eliminarTarea(ActionEvent event) {
-        Tarea tareaSeleccionada = listaTareasView.getSelectionModel().getSelectedItem();
+        ObservableList<Tarea> tareaSeleccionada = listaTareasView.getSelectionModel().getSelectedItems();
         if (tareaSeleccionada != null) {
-            tareas.remove(tareaSeleccionada);
-        }
+            
+                for(int i = 0; i < tareaSeleccionada.size(); i++){
+                 tareas.remove(tareaSeleccionada.get(i));
+                 }
+           }
     }
 
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         initialize();
+        listaTareasView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
     }    
     
 }
